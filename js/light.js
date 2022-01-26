5647b23171ca5150b56429a822f97882cdf4fae0
TomiiVaz <eliseovazquez01@gmail.com>
2022-01-25 23:40:59 -0300

    Se agrega local storage al modo noche

--- js/light.js
+++ js/light.js
@@ -1,12 +1,18 @@
 function hacerNoche() {
-  $("body").toggleClass("black", "body");
+  // $("body").toggleClass("black", "body");
+  $("body").removeClass("white");
+  $("body").addClass("black");
+
   $("#light-sun").hide();
   $(".light-moon").show();
   $("#name").css("color", "#fff");
 }
 
 function hacerDia() {
-  $("body").toggleClass("black", "body");
+  // $("body").toggleClass("black", "body");
+  $("body").removeClass("black");
+  $("body").addClass("white");
+
   $("#light-sun").show();
   $(".light-moon").hide();
   $("#name").css("color", "#000");
@@ -15,9 +21,19 @@ function hacerDia() {
 $("#light-sun").click(function (e) {
   e.preventDefault();
   hacerNoche();
+  localStorage.setItem("black-mode", "true");
 });
 
 $("#light-moon").click(function (e) {
   e.preventDefault();
   hacerDia();
+  localStorage.setItem("black-mode", "false");
 });
+
+// Obtenerlo
+
+if (localStorage.getItem("black-mode") == "true") {
+  hacerNoche();
+} else {
+  hacerDia();
+}
